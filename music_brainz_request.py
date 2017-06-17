9a5a36888ad817f6ca005ef4c71b9ea7fa6dc109
jhavaldar <havaldar@sas.upenn.edu>
2017-06-17 16:28:02 -0400

    First metric: song lengths vs billboard popularity

--- music_brainz_request.py
+++ music_brainz_request.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import musicbrainzngs
+import json
 
 def millisecond_format(num):
   m, s = divmod(num, 1000*60)
   return ("%d:%02d" % (m, s/1000))
 
+# Given an iter index in the loop, track name, and artist, does an API request to musicbrainz, and if 
+# that fails, then write the index + info to the log file
 def music_brainz_request(index, query_content, artist, log):
   
   musicbrainzngs.set_useragent("Billboard Analysis", "0.3", "http://jhavaldar.github.io/jhavaldar")
@@ -24,7 +27,8 @@ def music_brainz_request(index, query_content, artist, log):
   else:
     return "N/A"
 
-def search():
+#For each artist in the Billboard chart file, print out their lengths
+def create_Lengths_File():
 
   fpath = "us_billboard.psv"
   out_file = "us_billboard_lengths.psv"
@@ -50,5 +54,72 @@ def search():
   log.close()
   f.close()
 
-search()
+#For each entry in the Billboard track file, create a json of its chart history
+def toSerialDict():
+  data = {}
+  #Chart1 = This week position
+  #Date2 = This week's date
+  in_file_path = "us_billboard.psv"
+  df = pd.read_csv(in_file_path, sep='|')
+  total = df.shape[0]
 
+  for index, row in df.iterrows():
+    chart = str(row['Chart1']) # This week's position
+    date = str(row['Date2']) #This week's date
+
+    track = str(row['Track'])
+    artist = str(row['Artist'])
+    key = artist+"|"+track
+
+    if key in data.keys():
+      arr = data[key] # The array consisting of all the dates and position
+      arr[date]=chart
+    else:
+      data[key] = {date:chart}
+
+    print (index*100.0)/total;
+
+  with open('result.json', 'w') as fp:
+    json.dump(data, fp)
+
+def removeDoubles():
+  data = {}
+  #Chart1 = This week position
+  #Date2 = This week's date
+  in_file_path = "us_billboard_lengths.psv"
+  df = pd.read_csv(in_file_path, sep='|')
+  total = df.shape[0]
+
+  for index, row in df.iterrows():
+    chart = int(row['Chart']) # This week's position
+    track = str(row['Track'])
+    artist = str(row['Artist'])
+    length = str(row['Length'])
+    key = artist+"|"+track
+
+    # Check if the peak chart is less than the previously held one
+    if key in data.keys():
+      data[key]['Chart'] = min(chart, data[key]['Chart'])
+    else:
+      data[key] = {'Length': length, 'Chart': chart}
+
+    print (index*100.0)/total;
+
+  with open('lengths_chart.json', 'w') as fp:
+    json.dump(data, fp)
+
+def reFormat(filepath):
+  with open(filepath) as data_file:    
+    data = json.load(data_file)
+  newdata = []
+  for key in data:
+    length = data[key]['Length']
+    chart = data[key]['Chart']
+    artist, track =key.split("|")
+    arr = {'artist': artist, 'track':track, 'length': length, 'chart': chart}
+    newdata.append(arr)
+  with open('lengths_chart_v2.json', 'w') as fp:
+    json.dump(newdata, fp)
+
+removeDoubles()
+reFormat('lengths_chart.json')
